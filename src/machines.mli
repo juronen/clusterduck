99542f0e717f1fdb51ff4bee55c3e16d4a6f548d
juronen <juuso@debian.juuso.deb>
2016-04-14 00:36:20 -0400

    machines was an unnecessary module

--- src/machines.mli
+++
@@ -1,9 +0,0 @@
-open Core.Std
-
-type t = Host_and_port.t Queue.t
-
-(** [create] converts a list of (host * port) to a queue of Host_and_port.t *)
-val create : (string * int) list -> Host_and_port.t Queue.t
-
-(** [pop_machine t] attempts to dequeue a machine from t. *)
-val pop_machine : 'a Queue.t -> 'a option
