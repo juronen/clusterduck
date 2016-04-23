e1ef543755fabe86d8cbeccb780f9dd9d9b5e418
juronen <juuso@debian.juuso.deb>
2016-04-23 02:07:57 -0400

    moved parallel sig to builder.ml

--- src/builder.ml
+++ src/builder.ml
@@ -2,6 +2,57 @@ open Core.Std
 open Async.Std
 open Rpc_parallel_core_deprecated.Std
 
+module type Parallel_sig = sig
+
+  open Parallel_deprecated
+  open Worker_types
+
+  type worker_id
+
+  val spawn_worker
+    :  ?where : [`Local | `Remote of _ Remote_executable.t]
+    -> ?disown : bool
+    -> ?env : (string * string) list
+    -> ?rpc_max_message_size : int
+    -> ?rpc_handshake_timeout : Time.Span.t
+    -> ?rpc_heartbeat_config : Rpc.Connection.Heartbeat_config.t
+    -> ?connection_timeout : Time.Span.t
+    -> ?cd : string
+    -> ?umask : int
+    -> redirect_stdout : Fd_redirection.t
+    -> redirect_stderr : Fd_redirection.t
+    -> worker_arg
+    -> on_failure : (Error.t -> unit)
+    -> (worker_ret * worker_id) Or_error.t Deferred.t
+
+  val spawn_worker_exn
+    :  ?where : [`Local | `Remote of _ Remote_executable.t]
+    -> ?disown : bool
+    -> ?env : (string * string) list
+    -> ?rpc_max_message_size : int
+    -> ?rpc_handshake_timeout : Time.Span.t
+    -> ?rpc_heartbeat_config : Rpc.Connection.Heartbeat_config.t
+    -> ?connection_timeout : Time.Span.t
+    -> ?cd : string
+    -> ?umask : int
+    -> redirect_stdout : Fd_redirection.t
+    -> redirect_stderr : Fd_redirection.t
+    -> worker_arg
+    -> on_failure : (Error.t -> unit)
+    -> (worker_ret * worker_id) Deferred.t
+
+  val kill_worker : worker_id -> unit Or_error.t Deferred.t
+
+  val run
+    :  ?rpc_max_message_size : int
+    -> ?rpc_handshake_timeout : Time.Span.t
+    -> ?rpc_heartbeat_config : Rpc.Connection.Heartbeat_config.t
+    -> ?where_to_listen:Tcp.Where_to_listen.inet
+    -> Command.t
+    -> unit
+
+end;;
+
 module Network = struct
 
   type t =
@@ -11,7 +62,6 @@ module Network = struct
     } 
 
 end;;
-
 
 module Any_worker = struct
 
@@ -233,7 +283,7 @@ let build_network t =
   Network.
     { spouts
     ; workers
-    ; spawner = (module Spawner : Launch.Parallel_sig)
+    ; spawner = (module Spawner : Parallel_sig)
     }
 ;;
 
